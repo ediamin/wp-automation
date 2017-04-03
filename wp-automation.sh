10d41d3ae2cb7e1d51d41983daf5720f05f9b742
Edi Amin <to.ediamin@gmail.com>
2017-04-03 07:53:54 +0600

    Update credentials and add php path for MAMP user

--- wp-automation.sh
+++ wp-automation.sh
@@ -2,6 +2,9 @@
 
 clear
 
+# for MAMP user using php version 7.1.1 this is required
+# export PATH="/Applications/MAMP/bin/php/php7.1.1/bin:$PATH"
+
 # Remove/Uninstall process
 if [[ $1 == "remove" ]]; then
 
@@ -13,7 +16,7 @@ if [[ $1 == "remove" ]]; then
 		projectname=$2
 	fi
 
-	projectDirectory="/home/vagrant/www/$projectname"
+	projectDirectory="YOUR_WWW_ROOT_DIRECTORY_PATH/$projectname"
 
 	if [[ ! -d $projectDirectory ]]; then
 		echo "$projectDirectory directory not exists"
@@ -41,14 +44,14 @@ else
 	projectname=$1
 fi
 
-mkdir -p "/home/vagrant/www/$projectname"
-cd "/home/vagrant/www/$projectname"
+mkdir -p "YOUR_WWW_ROOT_DIRECTORY_PATH/$projectname"
+cd "YOUR_WWW_ROOT_DIRECTORY_PATH/$projectname"
 
 # download the WordPress core files
 wp core download
 
 # create the wp-config file with our standard setup
-wp core config --dbname=$projectname --dbuser=root --dbpass=ediamin --extra-php <<PHP
+wp core config --dbname=$projectname --dbuser=YOUR_DB_USERNAME --dbpass=YOUR_DB_PASSWORD --extra-php <<PHP
 define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_LOG', true );
 define( 'SCRIPT_DEBUG', true );
@@ -56,7 +59,7 @@ PHP
 
 # create database, and install WordPress
 wp db create
-wp core install --url="http://localhost/$projectname" --title="WordPress Localhost Site" --admin_user="admin" --admin_password="ediamin" --admin_email="to.ediamin@gmail.com"
+wp core install --url="http://localhost/$projectname" --title="WordPress Localhost Site" --admin_user="WP_ADMIN_USERNAME" --admin_password="WP_ADMIN_PASSWORD" --admin_email="WP_ADMIN_EMAIL_ADDRESS" --skip-email
 
 # discourage search engines
 wp option update blog_public 0
@@ -78,12 +81,12 @@ wp plugin delete akismet
 wp plugin delete hello
 
 # install starter theme
-if [ ! -z "$2" ]; then
-	wp theme install ~/epsilon.zip --activate
-fi
+# if [ ! -z "$2" ]; then
+# 	wp theme install ~/epsilon.zip --activate
+# fi
 
 echo "================================================================="
 echo "Installation is complete. open browse http://localhost/$projectname/wp-admin/"
-echo "Admin User: admin"
-echo "Admin Pass: ediamin"
+echo "Admin User: WP_ADMIN_USERNAME"
+echo "Admin Pass: WP_ADMIN_PASSWORD"
 echo "================================================================="
